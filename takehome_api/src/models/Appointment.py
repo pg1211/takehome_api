27851b13ff37496d75cc3accf0cfec20d01c2045
pg1211 <143668413+pg1211@users.noreply.github.com>
2025-05-17 14:40:33 -0400

    added fields to Patient, Appointment, created associations.py

--- takehome_api/src/models/Appointment.py
+++ takehome_api/src/models/Appointment.py
@@ -3,4 +3,8 @@ from takehome_api.src.database import db
 
 class Appointment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # add additional fields as needed
+    patient = db.Column(db.String(200), nullable=False)
+    scheduled = db.Column(db.Boolean, nullable=False)
+    appointment_id = db.Column(db.String(100), nullable=False)
+    provider = db.Column(db.String(200), nullable=False)
+    time = db.Column(db.String(27), nullable=False)
--- takehome_api/src/models/Patient.py
+++ takehome_api/src/models/Patient.py
@@ -3,4 +3,7 @@ from takehome_api.src.database import db
 
 class Patient(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # add additional fields as needed
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    state = db.Column(db.String(2), nullable=False)
+    insurance = db.Column(db.String(100), nullable=False)
