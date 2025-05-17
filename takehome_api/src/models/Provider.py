219c7be7d9d0c89ed1a7d750133d8b825c635611
pg1211 <143668413+pg1211@users.noreply.github.com>
2025-05-17 14:49:38 -0400

    added schema for Providers, new associations

--- takehome_api/src/models/Provider.py
+++ takehome_api/src/models/Provider.py
@@ -1,6 +1,20 @@
 from takehome_api.src.database import db
+from takehome_api.src.models.associations import provider_states, provider_insurances, State, Insurance
 
 class Provider(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     # add additional fields as needed
+    states = db.relationship(
+        'State',
+        secondary=provider_states,
+        backref='licensed_providers',
+        lazy='dynamic'
+    )
+
+    insurances = db.relationship(
+        'Insurance',
+        secondary=provider_insurances,
+        backref='accepted_providers',
+        lazy='dynamic'
+    )
