b168a99010e3b37d20a07dd1342fb58a08bef8f3
pg1211 <143668413+pg1211@users.noreply.github.com>
2025-05-17 14:57:14 -0400

    Update main.py

--- takehome_api/src/main.py
+++ takehome_api/src/main.py
@@ -1,10 +1,15 @@
 from dotenv import load_dotenv
 
 load_dotenv(verbose=True)
-from flask import Flask, Response
+from flask import Flask, Response, request, jsonify
 from flask_cors import CORS
 from instance.config import APP_CONFIG
 from takehome_api.src.database import db
+
+from takehome_api.src.models.Provider import Provider
+from takehome_api.src.models.Patient import Patient
+from takehome_api.src.models.Appointment import Appointment
+from takehome_api.src.external import schedule_with_provider
 
 def create_app(config_name):
     app = Flask(__name__)
